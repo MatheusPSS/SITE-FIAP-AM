69986c55b3e2d67a513b48e0bcf6225cba1b5683
Matheus Pereira <matheuspereirasilvasouza@outlook.com>
2017-10-23 17:30:02 -0200

    nac quase pronta

--- Site_MVC_FinTech/Controllers/InvestidorController.cs
+++ Site_MVC_FinTech/Controllers/InvestidorController.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Site_MVC_FinTech.Controllers
 {
-    public class InvestidorController : Controller
+    public class InvestidorController : DefaultController
     {
         // GET: Investidor
         public ActionResult Index()
@@ -38,7 +39,7 @@ namespace Site_MVC_FinTech.Controllers
         [Authorize(Roles = "Admin")]
         public ActionResult Listar()
         {
-            var pacote = Repositorio.ListarPacote();
+            var pacote = Repositorio.ListarPacotes();
 
             return View(pacote);
         }
@@ -70,9 +71,88 @@ namespace Site_MVC_FinTech.Controllers
         }
 
         [Authorize(Roles = "Admin, Investidor")]
+        [HttpGet]
+        public ActionResult ConfirmaDados(int id)
+        {
+            Investidor invest = new Investidor();
+            invest.pacote = Repositorio.ListarPacote(id);
+            invest.pessoa = Repositorio.ListarPessoa(UserId);
+
+
+            var investidores = Repositorio.Listarinvestidores();
+
+
+            int qtdPct = 0;
+
+            foreach (var inv in investidores.Where(iv => iv.pacote.IDPacote == invest.pacote.IDPacote))
+            {
+                qtdPct += inv.Quantidade;
+            }
+
+            invest.pacote.QtdDisponivel = invest.pacote.QtdTotal - qtdPct;
+
+            return View(invest);
+        }
+
+        [Authorize(Roles = "Admin, Investidor")]
+        [HttpPost]
+        public ActionResult ConfirmaDados(Investidor model)
+        {
+            model.pessoa = Repositorio.ListarPessoa(UserId);
+            model.pacote = Repositorio.ListarPacote(model.pacote.IDPacote);
+
+            var investidores = Repositorio.Listarinvestidores();
+
+
+            int qtdPct = 0;
+
+            foreach (var inv in investidores.Where(iv => iv.pacote.IDPacote == model.pacote.IDPacote))
+            {
+                qtdPct += inv.Quantidade;
+            }
+
+            if ((model.pacote.QtdTotal - qtdPct) - model.Quantidade >= 0)
+            {
+
+                bool enviamensagem = Repositorio.InseriInvestidor(model);
+
+                if (enviamensagem)
+                    ViewBag.Mensagem = "Obrigado!";
+                else
+                    ViewBag.Mensagem = "Ops! Algo deu errado, tente novamente mais tarde.";
+            }
+            else
+            {
+                ViewBag.Mensagem = "Ops! Número de pacotes não disponível.";
+            }
+
+            return RedirectToAction("AreaRestrita");
+        }
+
+        //PESQUISA
+
+        [Authorize(Roles = "Admin, Investidor")]
+        [HttpGet]
         public ActionResult AreaRestrita()
         {
-            return View();
+            var investidor = Repositorio.FiltrarPacote();
+
+            if (investidor == null || ((List<Investidor>)investidor).Count == 0)
+                ViewBag.Mensagem = "Nenhum cliente cadastrado";
+
+            return View(investidor);
+        }
+
+        [Authorize(Roles = "Admin, Investidor")]
+        [HttpPost]
+        public ActionResult AreaRestrita(string texto, string combo)
+        {
+            var investidor = Repositorio.FiltrarPacote(texto, combo);
+
+            if (investidor == null || ((List<Investidor>)investidor).Count == 0)
+                ViewBag.Mensagem = "Nenhum cliente encontrado";
+
+            return View(investidor);
         }
     }
 }
